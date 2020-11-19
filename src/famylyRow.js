094ee611fabbafd27fc8f4d0dff5753dac746d5a
francisco-j <ffuentes@ucol.mx>
2020-11-19 16:56:52 -0600

    removed logo

--- src/famylyRow.js
+++ src/famylyRow.js
@@ -1,1 +1,24 @@
-export default function FamylyRow({ fa
+export default function FamylyRow({ family, members }) {
+
+    function getMemberElemnts() {
+        return members.map( ({title, name}) => {
+            return (
+                <span>
+                    <b> {title}: </b>
+                    {name}<br/>
+                </span>
+            )
+        })
+    }
+
+    return (<>
+        <td>
+            <span>
+                {family.name} Family
+            </span>
+        </td>
+        <td>
+            {getMemberElemnts()}
+        </td>
+    </>)
+}
